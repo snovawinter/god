543181257922568d502b5634a4d60bd42bede08b
Litvak <litvak.alexey@gmail.com>
2018-04-03 09:45:16 +0300

    Security Config + Liquibase ChangeLog

--- boot/src/main/java/ru/shmntk/god/boot/controller/PersonController.java
+++ boot/src/main/java/ru/shmntk/god/boot/controller/PersonController.java
@@ -1,6 +1,7 @@
 package ru.shmntk.god.boot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +19,12 @@ public class PersonController {
 
     @Autowired
     public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+	this.personRepository = personRepository;
     }
 
     @GetMapping("/{personId}")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public Optional<Person> getPerson(@PathVariable Integer personId) {
-        return personRepository.findById(personId);
+	return personRepository.findById(personId);
     }
 }
--- boot/src/main/resources/db/changelog/db.changelog.sql
+++ boot/src/main/resources/db/changelog/db.changelog.sql
@@ -1,5 +1,22 @@
 --liquibase formatted sql
+
 --changeset litvak:1
+CREATE TABLE PERSON
+(
+  PERSON_ID   INTEGER PRIMARY KEY NOT NULL,
+  EMAIL       VARCHAR(255),
+  FIRST_NAME  VARCHAR(255),
+  LAST_NAME   VARCHAR(255),
+  MIDDLE_NAME VARCHAR(255),
+  PHONE       VARCHAR(255)
+);
+--rollback DROP TABLE PERSON;
+
+--changeset litvak:2
+CREATE SEQUENCE PERSON_SEQ start with 1 increment by 1;
+--rollback DROP SEQUENCE PERSON_SEQ;
+
+--changeset litvak:3
 INSERT INTO PERSON(PERSON_ID, EMAIL, FIRST_NAME, LAST_NAME, MIDDLE_NAME, PHONE)
 values (PERSON_SEQ.NEXTVAL, 'litvak.alexey@gmail.com', 'Алексей', 'Литвак', 'Александрович', '+79262777650');
 --rollback delete from person;
